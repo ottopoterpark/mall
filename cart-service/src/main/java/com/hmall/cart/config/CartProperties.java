e55dff767c3510656ee916d6528108f39d25eb82
ottopoterpark <11608356+uniquealuo@user.noreply.gitee.com>
2025-02-04 21:48:07 +0800

    动态路由-网关配置

---
+++ cart-service/src/main/java/com/hmall/cart/config/CartProperties.java
@@ -0,0 +1,15 @@
+package com.hmall.cart.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author unique
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "hm.cart")
+public class CartProperties {
+    private Integer maxAmount;
+}
